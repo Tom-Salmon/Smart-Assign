92cac97999a8daad958ab64cb6740d22b9277cff
Tom Salmon <tomsalmon117@gmail.com>
2025-06-11 12:54:05 +0300

    Implement Kafka producer to send pizza orders messages

---
+++ producer/index.ts
@@ -0,0 +1,59 @@
+import { Kafka } from "kafkajs";
+import { v4 as uuidv4 } from "uuid";
+
+const kafka = new Kafka({
+    clientId: "pizza-notifier",
+    brokers: ["localhost:9092"],
+});
+
+const producer = kafka.producer();
+async function sendPizzaNotification() {
+    await producer.connect();
+    console.log("Producer connected");
+
+    const order = {
+        orderId: uuidv4(),
+        customerName: "Tom Salmon",
+        restaurantName: "Domino's Pizza",
+        size: "Large",
+        toppings: ["Pepperoni", "Mushrooms"],
+        timestamp: Date.now()
+    };
+    try {
+        await producer.send({
+            topic: "pizza-orders",
+            messages: [
+                {
+                    key: order.orderId,
+                    value: JSON.stringify(order),
+                },
+            ],
+        })
+        console.log("Message sent:", order);
+    } catch (error) {
+        console.error("Error sending message:", error);
+    }
+}
+sendPizzaNotification().catch(console.error);
+/*
+    setInterval(async () => {
+        const message = {
+            key: uuidv4(),
+            value: JSON.stringify({
+                orderId: uuidv4(),
+                status: "ready",
+                timestamp: new Date().toISOString(),
+            }),
+        };
+
+        try {
+            await producer.send({
+                topic: "pizza-orders",
+                messages: [message],
+            });
+            console.log("Message sent:", message);
+        } catch (error) {
+            console.error("Error sending message:", error);
+        }
+    }, 5000);
+}*/
